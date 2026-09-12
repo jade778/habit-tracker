8de8060189e02dc2f9900b3d417c33126bc56aa8
Jade Lau <jade_lau@mac.lan>
2026-09-12 11:48:57 -0700

    Add gitignore, add iOS standalone support

---
+++ dist/sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'habit-quest-v1'
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    caches.open(CACHE).then(async (cache) => {
+      const cached = await cache.match(event.request)
+      const network = fetch(event.request)
+        .then((response) => {
+          if (response.ok) cache.put(event.request, response.clone())
+          return response
+        })
+        .catch(() => cached)
+      return cached || network
+    })
+  )
+})
